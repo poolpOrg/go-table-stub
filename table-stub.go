91c0bef3cf5f6c5bde7d981167429136a51fa8b5
Gilles Chehade <gilles@poolp.org>
2020-05-27 00:18:02 +0200

    initial import

---
+++ table-stub.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/poolpOrg/go-opensmtpd/table"
+)
+
+func check(token string, service table.LookupService, key string) {
+	//table.Boolean(token, true)
+	//table.Boolean(token, false)
+	table.Failure(token)
+}
+
+func lookup(token string, service table.LookupService, key string) {
+	table.Result(token, "foobar")
+	//table.Failure(token)
+}
+
+func fetch(token string, service table.LookupService) {
+	table.Result(token, "foobar")
+	//table.Failure(token)
+}
+
+func main() {
+	table.OnCheck(check)
+	table.OnLookup(lookup)
+	table.OnFetch(fetch)
+	table.Dispatch()
+}
